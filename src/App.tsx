f96bd5d0f89e9c4719c147e84249ccc4f23172df
oded atias <oatias@baybridgedigital.com>
2024-10-13 15:25:01 +0300

    Creating the layout responsive

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,19 @@ const App: FC = () => {
       templateRows='auto 1fr auto'
       minHeight='100vh'
     >
-      <GridItem as="nav" colSpan={6}>
+      <GridItem as="nav" colSpan={6} backgroundColor={'red'}>
         Navbar
       </GridItem>
 
-      <GridItem as="aside" colSpan={2}>
+      <GridItem as="aside" colSpan={1} backgroundColor={'green'} display={{ base: 'none', lg: 'block' }}>
         Aside
       </GridItem>
 
-      <GridItem as="main" colSpan={4}>
+      <GridItem as="main" colSpan={{base : 6, lg: 5}} backgroundColor={'blue'}>
         Main
       </GridItem>
 
-      <GridItem as="footer" colSpan={6}>
+      <GridItem as="footer" colSpan={6} backgroundColor={'yellow'}>
         Footer
       </GridItem>
     </Grid>
