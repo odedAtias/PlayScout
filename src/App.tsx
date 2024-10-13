f56cdab649c681b90b62b971edf0bda52396f820
oded atias <oatias@baybridgedigital.com>
2024-10-13 08:10:58 +0300

    Creating the basic layout of the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { Grid, GridItem } from '@chakra-ui/react'
 import { FC } from 'react'
 
 const App: FC = () => {
   return (
-    <div>App</div>
+    <Grid
+      templateColumns='repeat(6, 1fr)'
+      templateRows='auto 1fr auto'
+      minHeight='100vh'
+    >
+      <GridItem as="nav" colSpan={6}>
+        Navbar
+      </GridItem>
+
+      <GridItem as="aside" colSpan={2}>
+        Aside
+      </GridItem>
+
+      <GridItem as="main" colSpan={4}>
+        Main
+      </GridItem>
+
+      <GridItem as="footer" colSpan={6}>
+        Footer
+      </GridItem>
+    </Grid>
   )
 }
 
