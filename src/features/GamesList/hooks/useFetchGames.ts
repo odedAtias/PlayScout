3e33f283ad15b28a28e8ec6899b3c8f5b5e7f5fa
oded atias <oatias@baybridgedigital.com>
2024-11-21 09:08:00 +0200

    Remove unused logs

--- src/features/GamesList/hooks/useFetchGames.ts
+++ src/features/GamesList/hooks/useFetchGames.ts
@@ -8,7 +8,6 @@ import { RootState } from '../../../store/store';
 
 export const useFetchGames = () => {
     const { selectedGenre, selectedPlatform, selctedOrderOption } = useSelector((state: RootState) => state.gamesParams);
-    console.info('========selectedOrderOption',selctedOrderOption);
     const { payload, isLoading, error } = useFetchData<GamesFetchResponse>(gamesService, { params: { genres: selectedGenre, parent_platforms: selectedPlatform, ordering: selctedOrderOption === "none" ? null : selctedOrderOption } }, [selectedGenre, selectedPlatform, selctedOrderOption]);
     const games = payload?.results || [];
     return { games, error, isLoading };
