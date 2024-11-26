b52f70c55638bd7cc01742825eb0bc6e9555a3bd
oded atias <oatias@baybridgedigital.com>
2024-11-26 08:11:47 +0200

    InfinityScrollingGames: Fixing the ts in useFetchGames

--- src/features/GamesList/hooks/useFetchGames.ts
+++ src/features/GamesList/hooks/useFetchGames.ts
@@ -5,10 +5,29 @@ import useFetchData from '../../../hooks/useFetchData';
 import gamesService from '../services/games-service';
 import { GamesFetchResponse } from '../types/games';
 import { RootState } from '../../../store/store';
+import { orderType } from '../../SortSelector/types/types';
+
+type ParamsDependencies = string | number | orderType | null;
+
+interface Params {
+    genres: number | null;
+    parent_platforms: number | null;
+    ordering: orderType | null;
+};
 
 export const useFetchGames = () => {
     const { selectedGenre, selectedPlatform, selctedOrderOption } = useSelector((state: RootState) => state.gamesParams);
-    const { payload, isLoading, error } = useFetchData<GamesFetchResponse>(gamesService, { params: { genres: selectedGenre, parent_platforms: selectedPlatform, ordering: selctedOrderOption === "none" ? null : selctedOrderOption } }, [selectedGenre, selectedPlatform, selctedOrderOption]);
-    const games = payload?.results || [];
-    return { games, error, isLoading };
+
+    const paramsDependencies: ParamsDependencies[] = [selectedGenre, selectedPlatform, selctedOrderOption];
+
+    const params: Params = {
+        genres: selectedGenre,
+        parent_platforms: selectedPlatform,
+        ordering: selctedOrderOption === "none" ? null : selctedOrderOption
+    }
+
+    const { payload, isLoading, error } = useFetchData<GamesFetchResponse>(gamesService, { params }, paramsDependencies);
+
+    const gamesData = payload?.results || [];
+    return { gamesData, error, isLoading };
 };
