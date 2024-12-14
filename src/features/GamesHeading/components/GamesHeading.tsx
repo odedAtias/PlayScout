f3cf2dacb6cad5649183de3affc25bf81b4c014d
oded atias <oatias@baybridgedigital.com>
2024-12-14 10:18:01 +0200

    GamesHeading: Simplify the title calculation

--- src/features/GamesHeading/components/GamesHeading.tsx
+++ src/features/GamesHeading/components/GamesHeading.tsx
@@ -9,7 +9,7 @@ import { RootState } from '../../../store/store'
 const GamesHeading: FC = () => {
     const { selectedPlatform, selectedGenre } = useSelector((state: RootState) => state.gamesParams);
 
-    const title = `${selectedPlatform ? selectedPlatform?.name : ''} ${selectedGenre ? selectedGenre?.name : ''} Games`;
+    const title: string = `${selectedPlatform?.name || ''} ${selectedGenre?.name || ''} Games`;
 
     return (
         <Heading as="h1" size="lg" marginLeft={20} marginBottom={"2%"}>{title}</Heading>
