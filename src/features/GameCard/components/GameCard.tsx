7a9d56d571692d0a14803a2f2a7d1aa2f7551f1d
oded atias <oatias@baybridgedigital.com>
2024-12-14 16:15:16 +0200

    GameCard: Fixing the alignment between the heading and the critic score

--- src/features/GameCard/components/GameCard.tsx
+++ src/features/GameCard/components/GameCard.tsx
@@ -14,16 +14,18 @@ interface Props {
 }
 
 const GameCard: FC<Props> = (props: Props) => {
-    const { game: { id, background_image, name, parent_platforms, metacritic } } = props;
+    const { game: { background_image, name, parent_platforms, metacritic } } = props;
     return (
-        <GameCardContainer >
+        <GameCardContainer>
             <CardImage imageUrl={background_image} imageAlt={`${name} image`} />
             <CardBody h='40%'>
-                <Heading fontSize={'2xl'} noOfLines={2}>{name}</Heading>
                 <HStack mt={2}>
-                    <PlatformsList platforms={parent_platforms} />
+                    <Heading fontSize={'2xl'} noOfLines={1}>{name}</Heading>
                     <Spacer />
                     <CriticScore score={metacritic} />
+                </HStack>
+                <HStack mt={2}>
+                    <PlatformsList platforms={parent_platforms} />
                 </HStack>
             </CardBody>
         </GameCardContainer>
