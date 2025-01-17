ca8df42f42527b749c5b4bbbf1f5282c28326962
oded atias <oatias@baybridgedigital.com>
2025-01-17 23:36:41 +0200

    Refactoring footer

--- src/layout/footer/components/Footer.tsx
+++ src/layout/footer/components/Footer.tsx
@@ -1,7 +1,10 @@
+// React imports
 import { FC } from 'react';
+// Third party libraries imports
 import { HStack, Text, useColorModeValue } from '@chakra-ui/react';
 
 export const Footer: FC = () => {
+    
     const selectedBackgroundColor = useColorModeValue('gray.100', 'gray.600');
 
     return (
@@ -10,12 +13,12 @@ export const Footer: FC = () => {
             alignItems="center"
             p={5}
             backgroundColor={selectedBackgroundColor}
-            position="fixed" // Fix it in place
-            bottom={0} // Position it at the bottom of the viewport
-            left={0} // Stretch it to the left edge
-            right={0} // Stretch it to the right edge
-            zIndex={1000} // Ensure it stays above other elements
-            boxShadow="0 -2px 5px rgba(0, 0, 0, 0.1)" // Optional shadow for aesthetics
+            position="fixed" 
+            bottom={0} 
+            left={0} 
+            right={0} 
+            zIndex={1000} 
+            boxShadow="0 -2px 5px rgba(0, 0, 0, 0.1)"
         >
             <Text textAlign="center" fontSize="1.2rem">
                 PlayScout | Oded Atias | Copyright © 2024 - All rights reserved
