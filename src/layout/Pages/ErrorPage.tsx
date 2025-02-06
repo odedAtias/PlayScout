4a02188c2be2e0507fbfdc4d3de953bf26cce3ba
oded atias <oatias@baybridgedigital.com>
2025-02-06 20:42:02 +0200

    Adding Error message to the error page

--- src/layout/Pages/ErrorPage.tsx
+++ src/layout/Pages/ErrorPage.tsx
@@ -2,24 +2,36 @@
 import { FC } from "react";
 // Third party libraries imports
 import { Box, Button, Heading, HStack, Text, useColorModeValue } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 // FS imports
 import { Footer, Navbar } from "layout";
+
+interface RouteError {
+    status?: number;
+    statusText?: string;
+    internal?: boolean;
+    data?: string;
+    error?: Record<string, unknown>;
+}
 
 const ErrorPage: FC = () => {
 
     const textColor = useColorModeValue("black", "white");
     const backgroundColor = useColorModeValue("white", "gray.700");
 
+    const { status, data } = useRouteError() as RouteError;
+    const errorMessage: string = data ? data?.replace(/^Error: /, '') : "";
+
     return (
         <>
             <Navbar />
             <HStack spacing={6} align="flex-start" mt="5%" mx="6.5%" flexDirection={{ base: 'column', lg: "row" }}>
-                <Heading as="h1" fontSize={{ base: "6rem", lg: "16rem" }} mb={0}>404</Heading>
+                <Heading as="h1" fontSize={{ base: "6rem", lg: "16rem" }} mb={0}>{status || "404"}</Heading>
                 <Box mt={{ lg: '14' }} ml={{ base: 4, lg: 10 }}>
-                    <Text fontSize={{ base: "3xl", lg: "2xl" }} fontWeight="medium">Hmm, that’s not right.</Text>
-                    <Text fontSize={{ base: "3xl", lg: "2xl" }} fontWeight="medium">Let’s get you back on track.</Text>
+                    <Text fontSize={{ base: "3xl", lg: "2xl" }} fontWeight="medium">Hmm, that’s not right ...</Text>
+                    <Text fontSize={{ base: "3xl", lg: "2xl" }} fontWeight="medium">{errorMessage}</Text>
+                    <Text fontSize={{ base: "3xl", lg: "2xl" }} fontWeight="medium">Let’s get you back on track:</Text>
                     <Button
                         mt={{ base: 10, lg: 8 }}
                         h={{ base: '20', lg: '16' }}
