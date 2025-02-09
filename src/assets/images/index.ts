e341be79acddd8b787e01185e9aea9a655784630
oded atias <oatias@baybridgedigital.com>
2025-02-09 12:36:12 +0200

    rename images names

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,3 @@
 // FS imports
-export { default as Logo } from 'assets/images/logo.png';
+export { default as Logo } from 'assets/images/Logo.png';
 export { default as Placeholder } from 'assets/images/Placeholder.png';
